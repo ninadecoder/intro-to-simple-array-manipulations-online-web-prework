2791081698c79448369b1041d773da2734593c70
Nina Kazzim <kazzim@icloud.com>
2019-05-30 21:35:11 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,52 @@
+def using_push(array, element)
+  array = []
+  array.push(element)
+end
+
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_arg(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shitf_with_arg
+  array.shift(2)
+end
+
+def using_concat(array, element)
+   array.concat(element)
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten!
+end
+
+def using_delete(array, element)
+  array.delete(element)
+end
+
+def using_delete_at(array, int)
+  array.delete_at(int)
+end
